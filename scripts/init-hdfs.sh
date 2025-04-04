386276306fb84d49f88a009674140f259e6897d4
Unai <ugomez@ideko.es>
2025-04-04 05:20:22 +0200

    adelanto

--- scripts/init-hdfs.sh
+++ scripts/init-hdfs.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-echo "Inicializando HDFS..."
-hdfs dfs -mkdir -p /user/hive/warehouse
-hdfs dfs -mkdir -p /tmp
-hdfs dfs -chmod g+w /user/hive/warehouse
-hdfs dfs -chmod g+w /tmp
+echo "Creating hdfs project structure..."
 
-# Cargar archivos AVRO
-hdfs dfs -mkdir -p /user/hive/avro_data
-hdfs dfs -put /data/userdata/*.avro /user/hive/avro_data/
+# Crear y asignar permisos en HDFS
+hdfs dfs -mkdir -p hdfs://namenode/user/hive
+hdfs dfs -chown hive hdfs://namenode/user/hive
+hdfs dfs -mkdir -p hdfs://namenode/user/hive/warehouse
+hdfs dfs -chown hive hdfs://namenode/user/hive/warehouse
+hdfs dfs -mkdir -p hdfs://namenode/home/hive
+hdfs dfs -chown hive hdfs://namenode/home/hive
+
+# Crear directorio en HDFS
+hdfs dfs -mkdir -p hdfs://namenode/user/hive/userdata/
+hdfs dfs -chown hive hdfs://namenode/user/hive/userdata/
+hdfs dfs -mkdir -p hdfs://namenode/user/hive/estructura/
+hdfs dfs -chown hive hdfs://namenode/user/hive/estructura/
+
+echo "Uploading data to hdfs..."
+
+# Subir archivos AVRO a HDFS
+hadoop fs -mkdir -p /user/hive/warehouse/usuarios
+hadoop fs -put /data/userdata/*.avro /user/hive/warehouse/usuarios/
+hadoop fs -chmod -R 777 /user/hive/warehouse/usuarios
+hdfs dfs -put /estructura/* hdfs://namenode/user/hive/estructura/
+
+echo "✅ Datos AVRO cargados en HDFS correctamente."
